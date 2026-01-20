b9990c7973bf3edca5cd53f33855ff4c2ff07948
Petar Dinev <p.dinev55@gmail.com>
2026-01-20 11:57:58 +0200

    Add dotenv library

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import express from 'express';
+import dotenv from 'dotenv'
 import expressConfig from './configs/express.js';
+import dbConfig from './configs/db.js';
 
-const PORT = 3000;
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+
 
 start()
 
 async function start() {
     const app = express();
 
+    await dbConfig(app, DB_CONNECTION_STRING);
     expressConfig(app);
 
     app.get('/', (req, res) => {
